073aaf09d9aa40fc11ef24b28351a3352afa250a
Gregor N. Purdy, Sr <gnp@acm.org>
2005-01-28 08:00:43 +0000

    Added Apache license comment and svn:keywords property to every Java file

--- src/com/gregorpurdy/codec/UTF8Codec.java
+++ src/com/gregorpurdy/codec/UTF8Codec.java
@@ -1,5 +1,19 @@
+/*
+ *  Copyright 2005 Gregor N. Purdy
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.gregorpurdy.codec;
-
 
 /**
  * This class implements an UTF-8 transcoder per the description
@@ -44,7 +58,7 @@ package com.gregorpurdy.codec;
  * about at http://en.wikipedia.org/wiki/UTF-8).
  * 
  * @author Gregor N. Purdy &lt;gregor@focusresearch.com&gt; http://www.gregorpurdy.com/gregor
- * @version $Id: $
+ * @version $Id$
  */
 public class UTF8Codec {
 
